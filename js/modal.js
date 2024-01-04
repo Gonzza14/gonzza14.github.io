ab0357ae0adcd3790682a6d6c726cab8d9233377
Gonzza14 <gonzza_@hotmail.com>
2024-01-03 18:59:07 -0600

    Agregando modal a los demas proyectos

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,63 @@
-// Get the modal
-var modal = document.getElementById("modal-proyecto-1");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-proyecto-1");
+var modal1 = document.getElementById("modal-proyecto-1");
+var modal2 = document.getElementById("modal-proyecto-2");
+var modal3 = document.getElementById("modal-proyecto-3");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var btn1 = document.getElementById("btn-proyecto-1");
+var btn2 = document.getElementById("btn-proyecto-2");
+var btn3 = document.getElementById("btn-proyecto-3");
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "flex";
+var span1 = document.getElementById("close-1");
+var span2 = document.getElementById("close-2");
+var span3 = document.getElementById("close-3");
+
+btn1.onclick = function () {
+  modal1.style.display = "flex";
   document.body.style.overflow = "hidden";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+span1.onclick = function () {
+  modal1.style.display = "none";
   document.body.style.overflow = "scroll";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+    document.body.style.overflow = "scroll";
+  }
+};
+
+btn2.onclick = function () {
+  modal2.style.display = "flex";
+  document.body.style.overflow = "hidden";
+};
+
+span2.onclick = function () {
+  modal2.style.display = "none";
+  document.body.style.overflow = "scroll";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+    document.body.style.overflow = "scroll";
+  }
+};
+
+btn3.onclick = function () {
+  modal3.style.display = "flex";
+  document.body.style.overflow = "hidden";
+};
+
+span3.onclick = function () {
+  modal3.style.display = "none";
+  document.body.style.overflow = "scroll";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
     document.body.style.overflow = "scroll";
   }
 };
