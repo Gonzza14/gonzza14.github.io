79b7517320b1a1133d9fa5f5b6fd83c37436a875
Gonzza14 <gonzza_@hotmail.com>
2024-01-04 16:40:44 -0600

    Version 1.0.0 del cv terminada

--- js/ancla.js
+++ js/ancla.js
@@ -1,24 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var menuItems = document.querySelectorAll('.main-nav__menu__item');
+document.addEventListener("DOMContentLoaded", function () {
+  var menuItems = document.querySelectorAll(".main-nav__menu__item");
 
-    menuItems.forEach(function (item) {
-      item.addEventListener('click', function (event) {
-        // Evitar que el enlace realice la navegación
-        event.preventDefault();
-
-        // Remover la clase 'active' de todos los elementos del menú
-        menuItems.forEach(function (item) {
-          item.classList.remove('active');
-        });
-
-        // Agregar la clase 'active' al elemento clicado
-        item.classList.add('active');
-
-        // Obtener el valor del atributo href y desplazarse a esa sección
-        var targetSectionId = item.querySelector('.main-nav__menu__item__link').getAttribute('href').substring(1);
-        document.getElementById(targetSectionId).scrollIntoView({
-          behavior: 'smooth'
-        });
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      // Evitar que el enlace realice la navegación
+      event.preventDefault();
+      // Obtener el valor del atributo href y desplazarse a esa sección
+      let targetSectionId = item
+        .querySelector(".main-nav__menu__item__link")
+        .getAttribute("href")
+        .substring(1);
+      document.getElementById(targetSectionId).scrollIntoView({
+        behavior: "smooth",
       });
     });
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var menuItems = document.querySelectorAll(".contact-section__item");
+
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+     // Evitar que el enlace realice la navegación
+     event.preventDefault();
+
+     // Obtener el valor del atributo href
+     let targetUrl = item.querySelector('.contact-section__link').getAttribute('href');
+
+     // Abrir la URL en una nueva pestaña
+     window.open(targetUrl, '_blank');
+    });
+  });
+});
--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-
 var modal1 = document.getElementById("modal-proyecto-1");
 var modal2 = document.getElementById("modal-proyecto-2");
 var modal3 = document.getElementById("modal-proyecto-3");
@@ -21,13 +20,6 @@ span1.onclick = function () {
   document.body.style.overflowY = "scroll";
 };
 
-window.onclick = function (event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-    document.body.style.overflowY = "scroll";
-  }
-};
-
 btn2.onclick = function () {
   modal2.style.display = "flex";
   document.body.style.overflowY = "hidden";
@@ -36,13 +28,6 @@ btn2.onclick = function () {
 span2.onclick = function () {
   modal2.style.display = "none";
   document.body.style.overflowY = "scroll";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-    document.body.style.overflowY = "scroll";
-  }
 };
 
 btn3.onclick = function () {
@@ -56,7 +41,13 @@ span3.onclick = function () {
 };
 
 window.onclick = function (event) {
-  if (event.target == modal3) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+    document.body.style.overflowY = "scroll";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+    document.body.style.overflowY = "scroll";
+  } else if (event.target == modal3) {
     modal3.style.display = "none";
     document.body.style.overflowY = "scroll";
   }
